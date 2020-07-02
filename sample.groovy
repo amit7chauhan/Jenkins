0a7ca34c41f60f3ee0157a9785e34f67ccdc2afe
KalluruS <>
2020-07-02 08:05:50 +0530

    Update jenkins/jenkins-seed-jobs/ Terraform Module

---
+++ sample.groovy
@@ -0,0 +1,17 @@
+pipelineJob("sample") {
+  description('sample')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps48/_git/jenkins-seed-jobs")
+          }
+          branch("*/master")
+        }
+      }
+      scriptPath("Jenkinsfile")
+    }
+  }
+}
+
